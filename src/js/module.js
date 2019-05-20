e9808aff4394e1d5fb71901413c4d60514ca3915
Juan Pablo <juamportiz@gmail.com>
2019-05-20 01:14:45 -0300

    Terminado

---
+++ src/js/module.js
@@ -0,0 +1,70 @@
+/**
+ * Acá escribo la lógica de mi módulo
+ */
+// alert("Hola Bootcamp!");
+
+// Ejemplo de clase
+// class Bootcamp {
+//   hola() {
+//     console.log('Hola desde la consola')
+//   }
+// }
+// let b = new Bootcamp();
+// b.hola();
+
+
+// Ejemplo de función asíncrona
+// async function test() {
+//   return 'Este es el resultado!';
+// }
+
+// Usando then
+// test().then(function (result) {
+//   console.log(result)
+// })
+
+// usando await
+// async function callTest() {
+//   let result = await test();
+//   console.log(result);
+// }
+
+// callTest();
+
+class Contact {
+
+  constructor(name, email, tech, message) {
+    this.Name = name;
+    this.email = email;
+    this.Tech = tech;
+    this.Message = message;
+  }
+
+
+}
+
+// var local = localStorage.getItem('Contacts') ? JSON.parse(localStorage.getItem('Contacts')) : []; 
+var form = document.getElementById('contact-form');
+const modal = document.getElementById('modal-dialog');
+const closeButton = document.getElementById('close-modal');
+
+function toggleModal() {
+  // hide or show the modal
+  modal.classList.toggle('hidden');
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var name = document.getElementById('Name').value;
+  var email = document.getElementById('Email').value;
+  var tech = document.getElementById('Tech').value;
+  var message = document.getElementById('Message').value;
+
+  var contact = new Contact(name, email, tech, message);
+  console.log(contact);
+  localStorage.setItem('Contacts', JSON.stringify(contact));
+
+  form.reset();
+  toggleModal();
+  closeButton.addEventListener('click', toggleModal);
+})
